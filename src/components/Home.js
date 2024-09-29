c75b9d1498bf28b459bf0571da7be8dbe3069cab
daniel ung <daniel.ung@sjsu.edu>
2024-09-29 13:22:33 -0700

    Add src/components/Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Hospital App</h2>
+      <p>Manage appointments and patient information.</p>
+    </div>
+  );
+};
+
+export default Home;
